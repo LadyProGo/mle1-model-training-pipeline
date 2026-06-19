a4e47de1c6a6223c78200df7fa5a28b629c5fde1
Tatiana <mars1884@hotmail.com>
2026-06-19 15:56:20 +0000

    Implement create table task in Airflow DAG

--- part1_airflow/dags/collect_real_estate_data.py
+++ part1_airflow/dags/collect_real_estate_data.py
@@ -1,7 +1,14 @@
+import os
 from datetime import datetime
 
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+POSTGRES_CONN_ID = os.getenv("POSTGRES_CONN_ID", "destination_db")
+TARGET_TABLE = "real_estate_dataset"
 
 
 with DAG(
@@ -12,9 +19,37 @@ with DAG(
     catchup=False,
     tags=["real_estate", "data_collection"],
 ) as dag:
-    create_table = EmptyOperator(task_id="create_table")
+
+    @task(task_id="create_table")
+    def create_table() -> None:
+        create_table_sql = f"""
+        CREATE TABLE IF NOT EXISTS {TARGET_TABLE} (
+            flat_id INTEGER,
+            building_id INTEGER,
+            floor INTEGER,
+            kitchen_area FLOAT,
+            living_area FLOAT,
+            rooms INTEGER,
+            is_apartment BOOLEAN,
+            studio BOOLEAN,
+            total_area FLOAT,
+            price BIGINT,
+            build_year INTEGER,
+            building_type_int INTEGER,
+            latitude FLOAT,
+            longitude FLOAT,
+            ceiling_height FLOAT,
+            flats_count INTEGER,
+            floors_total INTEGER,
+            has_elevator BOOLEAN
+        );
+        """
+
+        postgres_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        postgres_hook.run(create_table_sql)
+
     extract = EmptyOperator(task_id="extract")
     transform = EmptyOperator(task_id="transform")
     load = EmptyOperator(task_id="load")
 
-    create_table >> extract >> transform >> load
+    create_table() >> extract >> transform >> load
